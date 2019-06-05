5aedae474aa8985a088cc1ae3511a31834b46446
shehjar <shehjar_sadhu@my.uri.edu>
2019-06-05 18:26:34 -0400

    ok fine dint want to comit this

--- multiple-seq-aigns/msa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 msa.py /Users/shehjarsadhu/Desktop/Summer19/HomologyDetectionResearch/Research-Proteins/multiple-seq-aigns/msa-data /Users/shehjarsadhu/Desktop/Summer19/HomologyDetectionResearch/Research-Proteins/multiple-seq-aigns/test_msa
